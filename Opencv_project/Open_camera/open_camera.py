b0c25f6cae2b7b7cfe5e7704d1c0002acaee4228
Jay-prakashpandey <81908427+Jay-prakashpandey@users.noreply.github.com>
2021-04-25 03:09:13 +0530

    Update open_camera.py

--- Opencv_project/Open_camera/open_camera.py
+++ Opencv_project/Open_camera/open_camera.py
@@ -1,3 +1,35 @@
+'''
+Step : 1
+Python 3 must be installed
+Check if these lib installed or not , if not run
+In cmd following command.
+a) pip install numpy
+b)pip install matplotlib
+C) pip install os-win
+D) pip install opencv-python
+E) pip install mediapipe
+F) pip install Pillow
+Step: 2
+Run/open with python the open_camera.py 
+Step:3 choose any of 3 modes i.e 1,2 or 3
+We have 3 mode :
+1 open camera by pressing 1 initially
+2 open file from gallery
+3 screen capture and record
+Step :4 press any of operations you want of given bellow:
+This is a open camera in which we can detect several things as given bellow:
+h = hand detection
+f = face detection
+e= eye detection
+m= motion detection
+c= object count
+v= Video recording
+P= photo taking
+q = closing and exit
+Arrow keys= for previous saved files and nevigation file.
+p = in file nevigation for parents folder
+Step:5 press q to exist ant any point
+'''
 import cv2
 import time
 import numpy as np
@@ -163,7 +195,7 @@ f=None
 t=7
 ch=None
 tim1=time.time()
-
+#Author @Jay prakash pandey
 while(True):
     if cf==1:
         img=captureScreen()
